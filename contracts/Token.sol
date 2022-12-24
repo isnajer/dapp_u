366f74da59437980ce81338116231f2a7942acdd
Isis Najera <isisnajer@gmail.com>
2022-12-24 15:24:13 -0800

    3. Testing Contracts

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,5 +4,17 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 contract Token {
-    string public name = "My Token";
+    string public name;
+    string public symbol;
+    uint256 public decimals = 18;
+    uint256 public totalSupply; // 1000000 * (10**decimals); // Scientific Notation - 1,000,000 x 10^18;
+
+    // Create constructor function that takes _name argument, save value to state variable in line 7:
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10**decimals);
+
+    }
 }
+
---
+++ test/Token.js
@@ -0,0 +1,56 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+
+
+// Old example:
+const tokens = (n) => {
+    return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+describe('Token', () => {
+    let token 
+
+    beforeEach(async () => {
+        const Token = await ethers.getContractFactory('Token')
+        token = await Token.deploy('Dapp U Token', 'DAPP', '1000000')
+    })
+
+    describe('Deployment', () => {
+        const name = 'Dapp U Token'
+        const symbol = 'DAPP'
+        const decimals = '18'
+        const totalSupply = tokens('1000000')
+    it('has correct name', async () => {
+        // Read token name:
+        // const name = await token.name()
+
+        // Check that name is correct - Use chai matchers:
+        // expect(name).to.equal("Dapp U Token")
+
+        // Cleaner code - 1 line:
+        expect(await token.name()).to.equal(name)
+    })
+
+    it('has correct symbol', async () => {
+        // Read token symbol:
+        // const symbol = await token.symbol()
+
+        // Check that symbol is correct - Use chai matchers:
+        // expect(symbol).to.equal("DAPP")
+
+        // Cleaner code - 1 Line:
+        expect(await token.symbol()).to.equal(symbol)
+    })
+
+    it('has correct decimals', async () => {
+        expect(await token.decimals()).to.equal(decimals)
+    })
+
+    it('has correct total supply', async () => {
+        expect(await token.totalSupply()).to.equal(totalSupply)
+    })
+
+    })
+
+})
