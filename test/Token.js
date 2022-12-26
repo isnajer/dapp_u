c8a40b8e383d46bb4f6e104860eee3a6b98d1f92
Isis Najera <isisnajer@gmail.com>
2022-12-26 00:18:43 -0800

    5. Token Transfers

--- test/Token.js
+++ test/Token.js
@@ -9,9 +9,7 @@ const tokens = (n) => {
 }
 
 describe('Token', () => {
-    let token, 
-        accounts, 
-        deployer
+    let token, accounts, deployer, receiver
 
     beforeEach(async () => {
         const Token = await ethers.getContractFactory('Token')
@@ -19,6 +17,7 @@ describe('Token', () => {
 
         accounts = await ethers.getSigners()
         deployer = accounts[0]
+        receiver = accounts[1]
     })
 
     describe('Deployment', () => {
@@ -26,40 +25,103 @@ describe('Token', () => {
         const symbol = 'DAPP'
         const decimals = '18'
         const totalSupply = tokens('1000000')
+    
 
-    it('has correct name', async () => {
-        // Read token name:
-        // const name = await token.name()
+        it('has correct name', async () => {
+            // Read token name:
+            // const name = await token.name()
 
-        // Check that name is correct - Use chai matchers:
-        // expect(name).to.equal("Dapp U Token")
+            // Check that name is correct - Use chai matchers:
+            // expect(name).to.equal("Dapp U Token")
 
-        // Cleaner code - 1 line:
-        expect(await token.name()).to.equal(name)
+            // Cleaner code - 1 line:
+            expect(await token.name()).to.equal(name)
+        })
+
+        it('has correct symbol', async () => {
+            // Read token symbol:
+            // const symbol = await token.symbol()
+
+            // Check that symbol is correct - Use chai matchers:
+            // expect(symbol).to.equal("DAPP")
+
+            // Cleaner code - 1 Line:
+            expect(await token.symbol()).to.equal(symbol)
+        })
+
+        it('has correct decimals', async () => {
+            expect(await token.decimals()).to.equal(decimals)
+        })
+
+        it('has correct total supply', async () => {
+            expect(await token.totalSupply()).to.equal(totalSupply)
+        })
+
+        it('it assigns total supply to deployer', async () => {
+            expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+        })
+
+        it('it assigns total supply to deployer', async () => {
+            expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+        })
+
     })
 
-    it('has correct symbol', async () => {
-        // Read token symbol:
-        // const symbol = await token.symbol()
+    describe('Sending Tokens', () => {
+        let amount, transaction, result
 
-        // Check that symbol is correct - Use chai matchers:
-        // expect(symbol).to.equal("DAPP")
+        describe('Success', () => {
 
-        // Cleaner code - 1 Line:
-        expect(await token.symbol()).to.equal(symbol)
-    })
+            beforeEach(async () => {
+                // Transfer tokens
+                amount = tokens(100)
+                transaction = await token.connect(deployer).transfer(receiver.address, amount)
+                result = await transaction.wait()
+            })
+    
+            it('transfers token balances', async () => {
+                // Log balance before transfer (if you want to see it in terminal):
+                // console.log("deployer balance before transfer", await token.balanceOf(deployer.address))
+                // console.log("receiver balance before transfer", await token.balanceOf(receiver.address))
+    
+    
+                // Ensure that tokens were tranfered (balance changed)
+                expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+                expect(await token.balanceOf(receiver.address)).to.equal(amount)
+    
+                // Log balance after transfer (if you want to see it in terminal):
+                // console.log("deployer balance after transfer", await token.balanceOf(deployer.address))
+                // console.log("receiver balance after transfer", await token.balanceOf(receiver.address))
+            })
+    
+            it('emits a Transfer event', async () => {
+                const event = result.events[0]
+                expect(event.event).to.equal('Transfer')
+    
+                const args = event.args
+                expect(args.from).to.equal(deployer.address)
+                expect(args.to).to.equal(receiver.address)
+                expect(args.value).to.equal(amount)
+                
+    
+            })
+        })
 
-    it('has correct decimals', async () => {
-        expect(await token.decimals()).to.equal(decimals)
-    })
+        describe('Failure', () => {
+            it('rejects insufficient balances', async () => {
+                // Transfer more tokens than deployer has - 100M
+                const invalidAmount = tokens(100000000)
+                await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted
+            })
 
-    it('has correct total supply', async () => {
-        expect(await token.totalSupply()).to.equal(totalSupply)
-    })
+            it('rejects invalid recepient', async () => {
+                const amount = tokens(100)
+                await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+            })
 
-    it('it assigns total supply to deployer', async () => {
-        expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
-    })
+        })
+
+
     })
 
 })
