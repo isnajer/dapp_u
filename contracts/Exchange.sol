bf1a66d0faf4769730a98375b73c99f7bb5ddd71
Isis Najera <isisnajer@gmail.com>
2023-01-04 15:24:55 -0600

    9. Deposit Tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -8,20 +8,40 @@ contract Exchange {
     // Account that receives exchange fees
     address public feeAccount;
     uint256 public feePercent;
+    mapping(address => mapping(address => uint256)) public tokens;
+
+    event Deposit(address token, address user, uint256 amount, uint256 balance);
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount= _feeAccount;
         feePercent = _feePercent;
     }
 
-    // Deposit Tokens
+
+    
+    // ----- DEPOSIT & WITHDRAW TOKEN -----
+
     function depositToken(address _token, uint256 _amount) public {
         // Transfer tokens to exchange
-        Token(_token).transferFrom(msg.sender, address(this), _amount);
+        require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+        // ^ 'require' wrapper adds extra layer of protection at the exchange level
         
-        // Update balance
-        // Emit an event
+        // Updates user balance
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
+
+        // Emit an event (from line 13)
+        emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+
     }
-    // Check Balances
+    
+    // Check Balances (wrapper function that checks if you have a mapping - bonus)
+    function balanceOf(address _token, address _user)
+        public
+        view
+        returns (uint256)
+    {
+        return tokens[_token][_user];
+    }
+
 
 }
